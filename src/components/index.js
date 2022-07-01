973fd8dbc3c072a98230281f8e4b1f8ab9bab068
LordUttam <sharmaji.uttam@gmail.com>
2022-07-01 14:48:31 +0530

    use absolute imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import { Navbar } from "./Navbar/Navbar";
-import { Search } from "./Search/Search";
-import { Editor } from "./Editor/Editor";
-import { Note } from "./Note/Note";
-import { Sidebar, SidebarMobile } from "./Sidebar/Sidebar";
+import { Navbar } from "components/Navbar/Navbar";
+import { Search } from "components/Search/Search";
+import { Editor } from "components/Editor/Editor";
+import { Note } from "components/Note/Note";
+import { Sidebar, SidebarMobile } from "components/Sidebar/Sidebar";
 export { Navbar, Search, Editor, Note, Sidebar, SidebarMobile };
